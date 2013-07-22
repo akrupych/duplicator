7742b56d7ea52f1c438280546baff487816ec3f8
andriy.krupych@gmail.com <andriy.krupych@gmail.com@add12a67-cd41-c041-6188-241bc9fdaa3a>
2013-07-22 21:04:05 +0000

    Moved to VS2010

--- trunk/Duplicator/Properties/AssemblyInfo.cs
+++ trunk/Duplicator/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("Duplicator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Duplicator")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
